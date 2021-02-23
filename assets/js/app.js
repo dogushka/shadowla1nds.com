8a25b9cc6218e53d2b9c3a533348b96a81a3fa79
dogushka <76619406+dogushka@users.noreply.github.com>
2021-02-23 20:03:39 +0300

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,106 @@
+$(function() {
+
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+
+    /* Fixed Header */
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function() {
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+    });
+
+    function checkScroll(scrollOffset) {
+        if( scrollOffset >= introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+
+
+    /* Smooth scroll */
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+
+        $("html, body").animate({
+            scrollTop:  blockOffset
+        }, 500);
+    });
+
+
+
+    /* Menu nav toggle */
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+
+
+
+    /* Collapse */
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+
+        $this.toggleClass("active");
+    });
+
+
+    /* Slider */
+    $("[data-slider]").slick({
+        infinite: true,
+        fade: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+});
+
+
+
+const loginPopup = document.querySelector(".login-popup");
+const close = document.querySelector(".close");
+
+
+window.addEventListener("load",function(){
+
+ showPopup();
+ // setTimeout(function(){
+ //   loginPopup.classList.add("show");
+ // },5000)
+
+})
+
+function showPopup(){
+      const timeLimit = 1 // seconds;
+      let i=0;
+      const timer = setInterval(function(){
+       i++;
+       if(i == timeLimit){
+        clearInterval(timer);
+        loginPopup.clas
+       } 
+      },1000);
+}
+
+
+close.addEventListener("click",function(){
+  loginPopup.classList.remove("show");
+})
